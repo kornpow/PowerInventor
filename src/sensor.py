7ecb3a3651a955991212df23ea2c81ef48ff519d
Sam Korn <korn94sam@gmail.com>
2017-06-19 22:03:11 -0600

    Got IO expander, db working now web server

--- src/sensor.py
+++ src/sensor.py
@@ -12,9 +12,14 @@ import smbus, os
 import sqlite3
 import sys
 
+
 import time
 import subprocess
 import requests
+
+
+from flask import Flask
+
 
 busNumber =  1 #int(os.getenv("I2C_BUS"))
 
@@ -32,67 +37,84 @@ inv1                = 5
 config0             = 6
 config1             = 7
 
-class SQL_DB:
+class WebServer():
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.app.run(host='0.0.0.0', port=80)
+
+    @app.route('/')
+    def hello_world():
+        return 'Hello World!'
+
+class SQL_DB():
 
     def __init__(self):
-        create_table()
+        self.create_table()
+        self.add_to_table(22,22,1,1)
 
-    def create_table():
-        print "creating"
+    def create_table(self):
+        
         conn = sqlite3.connect('GrowerApp.db')
 
         c = conn.cursor()
         #Create table
-        c.execute('''CREATE TABLE schedule 
-            (hour integer, minute integer ,outlet integer, onoff integer)''')
+        print "creating"
+        c.execute('''CREATE TABLE IF NOT EXISTS schedule 
+            (hour integer, min integer, outlet integer, onoff integer)''')
         conn.commit()
+        
 
-    def add_to_table(hour,min,outlet,onoff):
+    def add_to_table(self,hour,min,outlet,onoff):
         conn = sqlite3.connect('GrowerApp.db')
 
         c = conn.cursor()
-        pack = (hour,min,outlet,onoff)
+        pack = (hour,min, outlet,onoff)
+        # c.execute("SELECT * FROM schedule WHERE EXISTS",(chour,cmin) )
         c.execute("INSERT INTO schedule VALUES (?, ?, ?, ?)", pack )
         conn.commit()
 
-    def print_table():
+    def print_table(self):
         conn = sqlite3.connect('GrowerApp.db')
         c = conn.cursor()
         for row in c.execute('SELECT * FROM schedule ORDER BY hour'):
                 print row
 
-    def check_event():
-        conn = sqlite3.connect('GrowerApp.db')
-        c = conn.cursor()
-        t = time.localtime()
-        chour = t[3]
-        cmin = t[4]
-        print "Current hour is: " + str(chour)
-        print "Current minute is: " + str(cmin)
-        # for row in c.execute('SELECT * FROM schedule WHERE hour=?',(chour,) ):
-      #         print row   
-        c.execute('SELECT outlet,onoff FROM schedule WHERE hour=? AND minute=?',(chour,cmin) )
-        event_list = c.fetchall()
-        if(len(event_list) == 0):
-            print "There is no event occuring this minute!"
-            return
+    def check_event(self):
+        while True:
+            conn = sqlite3.connect('GrowerApp.db')
+            c = conn.cursor()
+            t = time.localtime()
+            chour = t[3]
+            cmin = t[4]
 
-        print "Event List ----->"
-        print event_list
-        outlet = event_list[0:][0][0]
-        onoff = event_list[0:][0][1]
-        print "Outlet to be toggled: " + str(outlet)
-        print "Parameter value sending to set function: " + str(outlet + onoff)
+            # print "Current hour is: " + str(chour)
+            # print "Current minute is: " + str(cmin)
+            # print "Current second is: " + str(csec)
+            # for row in c.execute('SELECT * FROM schedule WHERE hour=?',(chour,) ):
+          #         print row   
+            c.execute('SELECT outlet,onoff FROM schedule WHERE hour=? AND min=?',(chour,cmin) )
+            event_list = c.fetchall()
+            if(len(event_list) == 0):
+                print "There is no event occuring this minute!"
+                continue
+
+            print "Event List ----->"
+            print event_list
+            outlet = event_list[0:][0][0]
+            onoff = event_list[0:][0][1]
+            print "Outlet to be toggled: " + str(outlet)
+            print "Parameter value sending to set function: " + str(outlet + onoff)
+
 
      #      device_id = '3f003b000447343337373739'
         # access_token = 'fda31bbba7b0a1130f428cb3d87a2155e91e5b43'
         # args = str(outlet + onoff)
         # subprocess.call(["particle", "call", "DadSwitch", "set", str(outlet + onoff)])
 
-    def remove_event(hour,mins):
+    def remove_event(self,hour,mins):
         conn = sqlite3.connect('GrowerApp.db')
         c = conn.cursor()
-        c.execute('DELETE FROM schedule WHERE hour=? AND minute=?',(hour,mins))
+        c.execute('DELETE FROM schedule WHERE hour=? AND min=?',(hour,mins))
         conn.commit()
 
 class PCA9555:
@@ -144,28 +166,58 @@ class PCA9555:
 
         print portvals & 0xF0
 
+    def LED_Pattern(self):
+        self.writeOUT(1,0x11)
+        time.sleep(0.3)
+        # adxl345.writeOUT(0,0x03)
+        self.writeOUT(1,0x33)
+        time.sleep(0.2)
+        # adxl345.writeOUT(0,0x07)
+        self.writeOUT(1,0x77)
+        time.sleep(0.1)
+        # adxl345.writeOUT(0,0x0F)
+        self.writeOUT(1,0xFF)
+        time.sleep(1)
+        # adxl345.writeOUT(0,0x00)
+        self.writeOUT(1,0x00)
+        time.sleep(1.5)
+
 if __name__ == "__main__":
     # if run directly we'll just create an instance of the class and output
     # the current readings
+    #Start the web server
+    
+    #Start the GPIO expander 
     pca9555 = PCA9555()
+    #Connect and create database
     sql = SQL_DB()
-    sql.add_to_table(22,22,1,1)
+    #print database
+    sql.print_table()
+
+
+    # threading.Thread(target=sql.check_event()).start()
+
+    # threading.Thread(target=pca9555.LED_Pattern()).start()
+    
     while True:
+        pca9555.LED_Pattern()
+        print "Time is elapsing"
+        time.sleep(2)
         # print "INPUT:"
         # pca9555.readIN()
         # adxl345.writeOUT(0,0x01)
-        pca9555.writeOUT(1,0x11)
-        sleep(0.3)
-        # adxl345.writeOUT(0,0x03)
-        pca9555.writeOUT(1,0x33)
-        sleep(0.2)
-        # adxl345.writeOUT(0,0x07)
-        pca9555.writeOUT(1,0x77)
-        sleep(0.1)
-        # adxl345.writeOUT(0,0x0F)
-        pca9555.writeOUT(1,0xFF)
-        sleep(1)
-        # adxl345.writeOUT(0,0x00)
-        pca9555.writeOUT(1,0x00)
-        sleep(1.5)
+        # pca9555.writeOUT(1,0x11)
+        # sleep(0.3)
+        # # adxl345.writeOUT(0,0x03)
+        # pca9555.writeOUT(1,0x33)
+        # sleep(0.2)
+        # # adxl345.writeOUT(0,0x07)
+        # pca9555.writeOUT(1,0x77)
+        # sleep(0.1)
+        # # adxl345.writeOUT(0,0x0F)
+        # pca9555.writeOUT(1,0xFF)
+        # sleep(1)
+        # # adxl345.writeOUT(0,0x00)
+        # pca9555.writeOUT(1,0x00)
+        # sleep(1.5)
 
