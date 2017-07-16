16812aa9743b7eeff5cc6c4ffb646d1ad5cfd7ab
Sam Korn <korn94sam@gmail.com>
2017-07-16 13:36:55 -0400

    Fixing time zone issue

---
+++ startup.sh
@@ -0,0 +1,9 @@
+# Default to UTC if no TIMEZONE env variable is set
+echo "Setting time zone to ${TIMEZONE=UTC}"
+# This only works on Debian-based images
+echo "${TIMEZONE}" > /etc/timezone
+dpkg-reconfigure tzdata
+
+python create_server.py
+
+date
